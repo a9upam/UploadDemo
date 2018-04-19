058e9c2e50666010498515e492708f7307adbfc2
Anupam Gupta <a.anupamgupta@gmail.com>
2018-04-19 10:50:52 +0530

    Initial Commit

---
+++ UploadDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UploadDemo
+//
+//  Created by Anupam G on 19/04/18.
+//  Copyright © 2018 Anupam G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
